dde6e57a2bab138ee8be77e5b32c2d541962b7bc
Ketan Mittal <mittal.ketan1@gmail.com>
2022-07-26 09:43:47 -0700

    Update files

--- Exploratory Data Analysis/Logistic Regression.R
+++ Exploratory Data Analysis/Logistic Regression.R
@@ -1,15 +1,16 @@
 alzheimer_data <- readr::read_csv("https://raw.githubusercontent.com/COSMOS-DataScience/slides/main/data/alzheimer_data.csv")
 alz=alzheimer_data
 attach(alz)
+library(tidyverse)
 
 alz <- 
   alz %>% 
-  mutate(diagnosis.new = case_when(
-    hallsev == 0 ~0,
-    hallsev != 0 ~ 1
+  mutate(diagnosis = case_when(
+    diagnosis == 0 ~0,
+    diagnosis != 0 ~ 1
   ))
 
-diag.new<-as.factor(diagnosis.new)
+diag.new<- as.factor(diagnosis)
 
-diag.logistic<-glm(diag.new~as.factor(delsev)+as.factor(hallsev)+as.factor(agitsev)+as.factor(depdsev) + as.factor(anxsev) + as.factor(elatsev) + as.factor(apasev) + as.factor(disnsev) + as.factor(irrsev),family=binomial)
+diag.logistic<-glm(diag.new~delsev+ (hallsev)+ (agitsev)+ (depdsev) +  (anxsev) +  (elatsev) +  (apasev) +  (disnsev) +  (irrsev),family=binomial)
 summary(diag.logistic)
